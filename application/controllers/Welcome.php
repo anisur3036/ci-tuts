23c5cd99b8cef8dfac2ce021d11010268c627828
Anisur Rahman <anisurlist3@gmail.com>
2018-05-13 17:38:04 +0600

    প্রথম ধাপ validation complete

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,6 +6,35 @@ class Welcome extends My_Controller {
 	public function index() {
 		$subjects = $this->db->query('select * from subjects')->result();
 
-		echo $this->blade->view()->make('welcome_message', ['subjects' => $subjects]);
+		$this->form_validation->set_rules(
+			'email', 'Email',
+			'trim|required|valid_email',
+			[
+				//'required' => "%s is required.",
+			]
+		);
+
+		$this->form_validation->set_rules(
+			'password', 'Password',
+			'trim|required',
+			[
+				//'required' => "%s is required.",
+			]
+		);
+
+		$this->form_validation->set_rules(
+			'confpassword', 'Confrim Password',
+			'trim|required|matches[password]',
+			[
+				//'required' => "%s is required.",
+			]
+		);
+
+		if ($this->form_validation->run() == FALSE) {
+			echo $this->blade->view()->make('welcome_message', ['subjects' => $subjects]);
+		} else {
+			echo "success";
+		}
+
 	}
 }
--- application/views/welcome_message.blade.php
+++ application/views/welcome_message.blade.php
@@ -5,6 +5,30 @@
 	<h1 style="text-align: center;"><span class="glyphicon glyphicon-user"></span>  Welcome to CodeIgniter!</span></h1>
 
 	<div class="col col-md-4 col-md-offset-4">
+
+			<form action="" method="POST" role="form">
+
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="text" class="form-control" id="email" name="email" value="{{ set_value('email') }}">
+					<span class="help-block">{!! form_error('email') !!}</span>
+				</div>
+
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" class="form-control" id="password" name="password">
+					<span class="help-block">{!! form_error('password') !!}</span>
+				</div>
+				<div class="form-group">
+					<label for="confpassword">Confirm Password</label>
+					<input type="password" class="form-control" id="confpassword" name="confpassword">
+					<span class="help-block">{!! form_error('confpassword') !!}</span>
+				</div>
+
+
+
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</form>
 		<a href="user_guide/">User Guide</a>
 		<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 
